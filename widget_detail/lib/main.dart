5389bb61f88f365cd1535d8662b0337d477e24ec
Ali Akyıldız <ali.akyildiz.94@gmail.com>
2024-11-13 15:17:13 +0300

    - Text widget style çalışmaları. - Hex Color kütüphanesi yüklendi.

--- widget_detail/lib/main.dart
+++ widget_detail/lib/main.dart
@@ -1,27 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:widget_detail/constants/colors.dart';
 
 void main() {
-  runApp(const Test());
+  runApp(const WidgetDetail());
 }
 
-class WidgetDetail extends StatelessWidget {
+// class WidgetDetail extends StatelessWidget {
+//   const WidgetDetail({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("data"),
+//           centerTitle: true,
+//           backgroundColor: Colors.cyan,
+//           leading: const Text("Menu"),
+//         ),
+//         body: const Center(
+//           child: Text("Hello Word"),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {},
+//           child: const Text("+"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class WidgetDetail extends StatefulWidget {
   const WidgetDetail({super.key});
 
   @override
+  State<WidgetDetail> createState() => _WidgetDetailState();
+}
+
+class _WidgetDetailState extends State<WidgetDetail> {
+  int value = 0;
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("data"),
+          title: const Text("Hello Word"),
           centerTitle: true,
-          backgroundColor: Colors.cyan,
+          backgroundColor: HexColor(primaryColor),
           leading: const Text("Menu"),
         ),
-        body: const Center(
-          child: Text("Hello Word"),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(value.toString()),
+              Text(
+                "Body",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 10,
+                  color: HexColor(secondaryColor),
+                ),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              value += 1;
+            });
+          },
           child: const Text("+"),
         ),
       ),
@@ -55,7 +107,8 @@ class _TestState extends State<Test> {
             Text(value.toString()),
             ElevatedButton(
               onPressed: () {
-                setState(() { // --> Değişikliklerin ekrana yansımasını sağlar.
+                setState(() {
+                  // --> Değişikliklerin ekrana yansımasını sağlar.
                   testMethod();
                 });
               },
